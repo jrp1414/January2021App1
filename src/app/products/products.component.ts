7bd9bf8f370f21c65dd04f173268013ef34686b6
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2021-01-18 11:46:53 +0530

    Built In Directives examples

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, productList } from '../services/product.data';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ["./products.component.css"]
+})
+export class ProductsComponent {
+  products: Product[] = productList;
+  filterText: string = "";
+  constructor() {
+    // for (let key in this.products) {
+    //   console.log(key);
+    //   console.log(this.products[key]);
+    // }
+
+    // for (let prod of this.products) {
+    //   console.log(prod);
+    // }
+  }
+
+  getStyles(prod) {
+    if(prod.starRating>3){
+      return {backgroundColor:"chartreuse",color:"blue"};
+    }
+    return {backgroundColor:"crimson",color:"white"};
+  }
+
+  getClass(prod){
+    if (prod.starRating>3) {
+        // return "green bold";
+        return ["green","bold"];
+    }
+    return "red";
+  }
+
+}
